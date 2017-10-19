7c28bd40ac934fe094d268b46b531297be90e8c1
mamorky <andres_ball@hotmail.com>
2017-10-19 20:13:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mamorky/constraintlayoutinventory/InventoryConstraintLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.example.mamorky.constraintlayoutinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_constraint_layout);
+    }
+}
